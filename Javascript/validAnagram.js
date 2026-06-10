d846e31046fb68bc3a626bb350999c21ce09a092
TanviSurve0005 <tanvisurve1805@gmail.com>
2026-06-11 00:57:13 +0530

    Refactor containsDuplicate function and add isAnagram implementation in JavaScript

---
+++ Javascript/validAnagram.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if(s.length !== t.length) return false
+
+    const map= new Map()
+    for (const c of s){
+        if(map.has(c)) map.set(c, map.get(c)+1)
+        else map.set(c,1)
+    }
+
+    for (const c of t){
+        if(!map.has(c)) return false
+        map.set(c, map.get(c)-1)
+        if(map.get(c) === 0) map.delete(c)
+    }
+    if(map.size > 0) return false
+
+    return true
+};
+
+console.log(isAnagram("anagram", "nagaram")); 
+console.log(isAnagram("rat", "car")); 
+// Big O(n)
