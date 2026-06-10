ce2072ac1acdccda81642c4d07a35146a6e89e94
TanviSurve0005 <tanvisurve1805@gmail.com>
2026-06-11 00:33:22 +0530

    Add implementation of containsDuplicate function in JavaScript

---
+++ Javascript/containsDuplicate.js
@@ -0,0 +1,13 @@
+var containsDuplicate = function(nums) {
+    const map = new Map();
+    for(const n of nums){
+        if(map.has(n)) return true
+        map.set(n, true)
+    }
+    return false;
+};
+
+console.log(containsDuplicate([1,2,3,1])); 
+console.log(containsDuplicate([1,2,3,4]));
+
+// Big O(n)
