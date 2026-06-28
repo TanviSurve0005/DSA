d34e778a001ec16706a54ee87000d289eb1231a6
TanviSurve0005 <tanvisurve1805@gmail.com>
2026-06-29 01:41:29 +0530

    Add star pattern generation script

---
+++ Python/star_problems.py
@@ -0,0 +1,26 @@
+n = 5
+print("Right Tringle of star")
+for i in range(1, n+1):
+    print("*" * i)
+
+print("Inverted triangle")
+for i in range(n, 0, -1):
+    print("*" * i)
+
+print("Pyramid(centered)")
+for i in range(1, n+1):
+    print(" " * (n-i) + "*" * (2*i-1))
+
+print("Number Triangle")
+for i in range(1, n+1):
+    
+    for j in range(n, i+1):
+        print(j, end= " ")
+    print()
+
+print("Number Triangle centered")
+for i in range(1, n+1):
+    print(" " * (n - i), end="")
+    for j in range(1, i+1):
+        print(j, end= " ")
+    print()
