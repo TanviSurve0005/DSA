3f3f6d484865570ca4199f544c0cde2732c44bbe
TanviSurve0005 <tanvisurve1805@gmail.com>
2026-07-02 01:54:40 +0530

    Update star pattern generation script to include additional patterns and enhance functionality

--- Python/star_problems.py
+++ Python/star_problems.py
@@ -24,3 +24,10 @@ for i in range(1, n+1):
     for j in range(1, i+1):
         print(j, end= " ")
     print()
+
+#Dimond
+print("Dimond Pattern")
+for i in range(1, n+1):
+    print(" " * (n-i) + "*" * (2*i-1))
+for i in range(n, 0, -1):
+    print(" " * (n-i) + "*" * (2*i-1))
